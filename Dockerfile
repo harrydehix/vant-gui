1ff781bf49d4c05f9ac77dd321110723cd292fc8
harrydehix <harry.holly02@gmail.com>
2025-03-02 13:12:08 +0100

    Fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:18
 WORKDIR /gui
+COPY package-lock.json package-lock.json
 COPY package.json package.json
 RUN npm install
 COPY public public
@@ -7,10 +8,8 @@ COPY src src
 COPY tsconfig.json tsconfig.json
 COPY .envdocker .env
 RUN npm run build
-WORKDIR /etc/ssl/certs/
-COPY public.crt ./public.crt
-COPY private.key ./private.key
-WORKDIR /gui
+COPY public.crt public.crt
+COPY private.key private.key
 COPY server.js server.js
 CMD node server.js
 EXPOSE 443
--- src/live-weather/useLiveWeather.tsx
+++ src/live-weather/useLiveWeather.tsx
@@ -5,99 +5,111 @@ import LiveWeatherData from "./LiveWeatherData";
 import dateFormat from "dateformat";
 import LiveWeatherProps from "./LiveWeatherProps";
 
-const useLiveWeather = (options : LiveWeatherProps) => {
+const useLiveWeather = (options: LiveWeatherProps) => {
     const [failedUpdates, setFailedUpdates] = useState<number>(0);
     const [connected, setConnected] = useState<boolean>(false);
-    const [currentConditions, setCurrentConditions] = useState<CurrentConditions>({
-        altimeter: null,
-        chill: null,
-        consoleBatteryVoltage: null,
-        dewpoint: null,
-        etDay: null,
-        etMonth: null,
-        etYear: null,
-        forecast: null,
-        forecastID: null,
-        forecastRule: null,
-        heat: null,
-        humExtra:
-            [null, null, null, null, null, null, null],
-        humIn: null,
-        humOut: null,
-        leafTemps:
-            [null, null, null, null],
-        leafWetnesses:
-            [null, null, null, null],
-        press: null,
-        pressAbs: null,
-        pressCalibrationOffset: null,
-        pressRaw: null,
-        pressReductionMethod: null,
-        pressReductionMethodID: null,
-        pressTrend: null,
-        pressTrendID: null,
-        pressUserOffset: null,
-        rain15m: null,
-        rain1h: null,
-        rain24h: null,
-        rainDay: null,
-        rainMonth: null,
-        rainRate: null,
-        rainYear: null,
-        soilMoistures:
-            [null, null, null, null],
-        soilTemps:
-            [null, null, null, null],
-        solarRadiation: null,
-        stormRain: null,
-        stormStartDate: null,
-        sunrise: null,
-        sunset: null,
-        tempExtra:
-            [null, null, null, null, null, null, null],
-        tempIn: null,
-        tempOut: null,
-        thsw: null,
-        time: null,
-        transmitterBatteryStatus: null,
-        uv: null,
-        wind: null,
-        windAvg10m: null,
-        windAvg2m: null,
-        windDir: null,
-        windDirDeg: null,
-        windGust: null,
-        windGustDir: null,
-        windGustDirDeg: null,
-    });
+    const [currentConditions, setCurrentConditions] =
+        useState<CurrentConditions>({
+            altimeter: null,
+            chill: null,
+            consoleBatteryVoltage: null,
+            dewpoint: null,
+            etDay: null,
+            etMonth: null,
+            etYear: null,
+            forecast: null,
+            forecastID: null,
+            forecastRule: null,
+            heat: null,
+            humExtra: [null, null, null, null, null, null, null],
+            humIn: null,
+            humOut: null,
+            leafTemps: [null, null, null, null],
+            leafWetnesses: [null, null, null, null],
+            press: null,
+            pressAbs: null,
+            pressCalibrationOffset: null,
+            pressRaw: null,
+            pressReductionMethod: null,
+            pressReductionMethodID: null,
+            pressTrend: null,
+            pressTrendID: null,
+            pressUserOffset: null,
+            rain15m: null,
+            rain1h: null,
+            rain24h: null,
+            rainDay: null,
+            rainMonth: null,
+            rainRate: null,
+            rainYear: null,
+            soilMoistures: [null, null, null, null],
+            soilTemps: [null, null, null, null],
+            solarRadiation: null,
+            stormRain: null,
+            stormStartDate: null,
+            sunrise: null,
+            sunset: null,
+            tempExtra: [null, null, null, null, null, null, null],
+            tempIn: null,
+            tempOut: null,
+            thsw: null,
+            time: null,
+            transmitterBatteryStatus: null,
+            uv: null,
+            wind: null,
+            windAvg10m: null,
+            windAvg2m: null,
+            windDir: null,
+            windDirDeg: null,
+            windGust: null,
+            windGustDir: null,
+            windGustDirDeg: null,
+        });
 
-    const current = (currentOptions: {field: keyof CurrentConditions, formatDateTime?: string, round?: boolean, precision?: number, index?: number, nullReplacementString?: string, nullReplacement?: React.ReactNode}) => {
+    const current = (currentOptions: {
+        field: keyof CurrentConditions;
+        formatDateTime?: string;
+        round?: boolean;
+        precision?: number;
+        index?: number;
+        nullReplacementString?: string;
+        nullReplacement?: React.ReactNode;
+    }) => {
         let result = currentConditions[currentOptions.field];
 
-        if(result instanceof Array){
+        if (result instanceof Array) {
             result = result[currentOptions.index || 0];
         }
 
         const raw = result;
 
-        if(typeof result === "string" && currentOptions.formatDateTime){
-            result = dateFormat(new Date(result), currentOptions.formatDateTime)
+        if (typeof result === "string" && currentOptions.formatDateTime) {
+            result = dateFormat(
+                new Date(result),
+                currentOptions.formatDateTime
+            );
         }
 
-        if(typeof result === "number"){
-            if(currentOptions.round){
+        if (typeof result === "number") {
+            if (currentOptions.round) {
                 result = Math.round(result);
-            }else if(currentOptions.precision && typeof result === "number"){
+            } else if (currentOptions.precision && typeof result === "number") {
                 result = result.toFixed(currentOptions.precision);
             }
         }
-        
 
         let node: React.ReactNode = result;
 
-        if(result === null){
-            node = currentOptions.nullReplacement !== undefined ? currentOptions.nullReplacement : options.nullReplacement;
-            result = (currentOptions.nullReplacementString !== undefined ? currentOptions.nullReplacementString : options.nullReplacementString)!;
+        if (result === null) {
+            node =
+                currentOptions.nullReplacement !== undefined
+                    ? currentOptions.nullReplacement
+                    : options.nullReplacement;
+            result = (
+                currentOptions.nullReplacementString !== undefined
+                    ? currentOptions.nullReplacementString
+                    : options.nullReplacementString
+            )!;
         }
 
         return {
@@ -105,21 +117,22 @@ const useLiveWeather = (options : LiveWeatherProps) => {
             str: result.toString(),
             node: node,
         };
-    }
+    };
 
     useEffect(() => {
-        function updateFail(){
+        function updateFail() {
             setFailedUpdates(failedUpdates + 1);
-            if(failedUpdates > options.maximumUpdateFailCount){
+            if (failedUpdates > options.maximumUpdateFailCount) {
                 setConnected(false);
             }
         }
 
-        function validUpdate(){
+        function validUpdate() {
             setFailedUpdates(0);
             setConnected(true);
         }
-        const interval = setInterval(() => {
+
+        function update() {
             console.log(`Requesting weather data from '${options.api}'!`);
             superagent
                 .get(options.api + "/v1/current")
@@ -128,38 +141,64 @@ const useLiveWeather = (options : LiveWeatherProps) => {
                 .query({ pressureUnit: options.units.pressure })
                 .query({ solarRadiationUnit: options.units.solarRadiation })
                 .query({ temperatureUnit: options.units.temperature })
-                .set('accept', 'json')
-                .set('x-api-key', options.apiKey)
+                .set("accept", "json")
+                .set("x-api-key", options.apiKey)
                 .timeout(1000)
-                .end((err?: superagent.ResponseError, res?: superagent.Response) => {
-                    if (!res || !res.ok) {
-                        console.error("Failed to get realtime record!");
-                        if (res && res.body && res.body.message) {
-                            console.error("Server message: '" + res.body.message + "'");
+                .end(
+                    (
+                        err?: superagent.ResponseError,
+                        res?: superagent.Response
+                    ) => {
+                        if (!res || !res.ok) {
+                            console.error("Failed to get realtime record!");
+                            if (res && res.body && res.body.message) {
+                                console.error(
+                                    "Server message: '" + res.body.message + "'"
+                                );
+                            } else {
+                                console.error("Is your api running?");
+                            }
+                            updateFail();
+                        } else if (res && res.body && res.body.data) {
+                            setCurrentConditions(res.body.data);
+                            validUpdate();
                         } else {
-                            console.error("Is your api running?");
+                            console.error("Failed to get realtime record!");
+                            updateFail();
                         }
-                        updateFail();
-                    } else if (res && res.body && res.body.data) {
-                        setCurrentConditions(res.body.data);
-                        validUpdate();
-                    } else {
-                        console.error("Failed to get realtime record!");
-                        updateFail();
                     }
-                });
-        }, options.interval! * 1000);
+                );
+        }
+        update();
+        const interval = setInterval(update, options.interval! * 1000);
 
         return () => clearInterval(interval);
-    }, [options.api, options.interval, options.apiKey, options.units.pressure, options.units.rain, options.units.solarRadiation, options.units.temperature, options.units.wind, options.maximumUpdateFailCount, failedUpdates]);
+    }, [
+        options.api,
+        options.interval,
+        options.apiKey,
+        options.units.pressure,
+        options.units.rain,
+        options.units.solarRadiation,
+        options.units.temperature,
+        options.units.wind,
+        options.maximumUpdateFailCount,
+        failedUpdates,
+    ]);
 
     return [connected, currentConditions, current] as [
         boolean,
-        CurrentConditions, 
-        (
-            options: {field: keyof CurrentConditions, formatDateTime?: string, round?: boolean, precision?: number, index?: number, nullReplacementString?: string, nullReplacement?: React.ReactNode}
-        ) => LiveWeatherData<any>
+        CurrentConditions,
+        (options: {
+            field: keyof CurrentConditions;
+            formatDateTime?: string;
+            round?: boolean;
+            precision?: number;
+            index?: number;
+            nullReplacementString?: string;
+            nullReplacement?: React.ReactNode;
+        }) => LiveWeatherData<any>
     ];
-}
+};
 
 export default useLiveWeather;
